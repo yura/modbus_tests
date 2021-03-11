639f2613a08de3ecb1aea3c04ce9c776a5b3b65f
Yury Kotlyarov <yura@brainhouse.ru>
2021-03-11 16:45:55 +0500

    feat: Haskell using modbus-tcp

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,67 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
-import qualified Data.ByteString    as C
-import           Network.Simple.TCP
+import           Control.Exception
+import           Data.IP                   (toHostAddress, IPv4)
+import           Data.Range                as R
+import           Data.Word                 (Word16)
+import qualified Network.Socket            as S
+import qualified Network.Socket.ByteString as S
+import qualified System.Timeout            as TM
 
-import Data.Word
+import qualified Network.Modbus.Protocol   as MB
+
+import           Network.Modbus.TCP
 
 main :: IO ()
 main = do
-  response <- runCommand "192.168.0.99" "502"
-  putStrLn $ show response
+    --socket <- getTCPSocket "192.168.0.99" 502 1000
+    socket <- connect $ getAddr (read "192.168.0.99" :: IPv4) 502
+    let config = getTCPConfig socket 1000000
+    rtn <- runSession directWorker config simpleSession
+    print rtn
 
-cmd :: [Word8]
-cmd = [0x00, 0x01, 0x00, 0x00, 0x00, 0x06, 0x01, 0x03, 0x0f, 0xe0, 0x00, 0x01]
+simpleSession :: Session IO [Word16]
+simpleSession =
+    readHoldingRegisters tpu range
 
-runCommand ipAddress port = connect ipAddress port $ \(connectionSocket, remoteAddr) -> do
-  putStrLn $ "Connection established to " ++ show remoteAddr
-  send connectionSocket $ C.pack cmd
-  msg <- recv connectionSocket 1024
-  case msg of
-    Nothing -> return C.empty
-    Just m  -> return m
+connect :: S.SockAddr -> IO S.Socket
+connect addr = do
+    putStrLn ("Connecting to " ++ show addr ++ "...")
+    s <- S.socket S.AF_INET S.Stream S.defaultProtocol
+    S.connect s addr
+    putStrLn "connected"
+    return s
 
+getAddr :: IPv4 -> Int -> S.SockAddr
+getAddr ip portNum = S.SockAddrInet (fromIntegral portNum) (toHostAddress ip)
+
+-- T: transactionId, P: protocolId, U: unitId
+tpu = TPU 1 ModbusTcp 1
+range = R.singleton $ Address 4064
+
+
+---------------
+
+
+-- Connect to the server using a new socket and checking for a timeout
+getTCPSocket :: IPv4 -> Int -> Int -> IO (Maybe S.Socket)
+getTCPSocket ip port tm = do
+    let addr = getAddr ip port
+    s <- S.socket S.AF_INET S.Stream S.defaultProtocol
+    (rlt :: Either SomeException (Maybe ())) <- try $ TM.timeout tm (S.connect s addr)
+    case rlt of
+        Left _ -> return Nothing
+        Right m -> return $ s <$ m
+
+getTCPConfig :: S.Socket -> Int -> Config
+getTCPConfig s timeout =
+    Config
+        { MB.cfgWrite = S.send s
+        , MB.cfgRead = S.recv s 4096
+        , MB.cfgCommandTimeout = timeout
+        , MB.cfgRetryWhen = const . const False
+        , MB.cfgEnableBroadcasts = False
+        }
